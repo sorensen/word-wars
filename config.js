8d870805fbd30045dfdb749fae9893563340a396
Beau Sorensen <mail@beausorensen.com>
2012-11-16 20:38:12 -0800

    Refactoring for OO, currently broken

--- config.js
+++ config.js
@@ -1,87 +1,18 @@
-var express     = require('express')
-  , connect     = require('connect')
-  , cookie      = require('cookie')
-  , redis       = require('redis')
-  , ejs         = require('ejs')
-  , sio         = require('socket.io')
-  , parseCookie = connect.utils.parseSignedCookies
-  , RedisStore  = require('connect-redis')(express)
 
-module.exports = function (app) {
-  var io = sio.listen(app.settings.server)
-
-  app.configure('development', function () {
-    app.set('redis', {
-        host: 'localhost'
-      , port: 6379
-    })
-    app.set('port', 3000)
-
-    // app.use(express.logger())
-  })
-
-  app.configure('production', function () {
-    app.set('redis', {
-        host: 'nodejitsudb2600402396.redis.irstack.com'
-      , port: 6379
-      , auth: 'nodejitsudb2600402396.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4'
-    })
-    app.set('port', 80)
-  })
-
-  app.configure(function () {
-    app.set('io', io)
-    app.set('db', redis.createClient(app.settings.redis.port, app.settings.redis.host))
-    app.settings.db.auth(app.settings.redis.auth)
-    app.set('sessionStore', new RedisStore({client: app.settings.db}))
-    app.set('sessionSecret', 'IvIVKmFkjE!!a3fP6C38%m%C0%n094bpGnn73GrJU5$oET6!tI^a4pmFs7X3!Ue^')
-    app.use(express.bodyParser())
-    app.use(express.methodOverride())
-    app.use(express.cookieParser())
-    app.use(express.session({
-        store: app.settings.sessionStore
-      , secret: app.settings.sessionSecret
-    }))
-    app.use(express.errorHandler({ 
-      dumpExceptions: true
-    , showStack: true
-    }))
-    app.use(express.static(__dirname + '/public'))
-    app.use(app.router)
-  })
-
-  io.configure(function () {
-    var RedisStore = sio.RedisStore
-
-    var redisPub = redis.createClient(app.settings.redis.port, app.settings.redis.host)
-      , redisSub = redis.createClient(app.settings.redis.port, app.settings.redis.host)
-    redisPub.auth(app.settings.redis.auth)
-    redisSub.auth(app.settings.redis.auth)
-
-    io.set('store', new RedisStore({
-        redis: redis
-      , redisPub: redisPub
-      , redisSub: redisSub
-      , redisClient: app.settings.db
-    }))
-    
-    io.disable('log');
-
-    io.set('authorization', function (data, callback) {
-      if (!data.headers.cookie) {
-        return callback('No cookie', false)
-      }
-
-      var parsedCookie = parseCookie(cookie.parse(decodeURIComponent(data.headers.cookie)), app.settings.sessionSecret)
-      app.settings.sessionStore.get(parsedCookie['connect.sid'], function (err, session) {
-        if (err || !session) {
-          return callback('Error', false)
-        }
-
-        data.session = session
-        data.session.id = parsedCookie['connect.sid']
-        callback(null, true)
-      })
-    })
-  })
+module.exports = {
+  development: {
+    port: 3000
+  , redis: {
+      host: 'localhost'
+    , port: 6379
+    }  
+  }
+, production: {
+    port: 80
+  , redis: {
+      host: 'nodejitsudb2600402396.redis.irstack.com'
+    , port: 6379
+    , auth: 'nodejitsudb2600402396.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4'
+    }
+  }
 }
--- server.js
+++ server.js
@@ -1,22 +1,106 @@
-if (process.env.NODE_ENV !== 'production') require('rconsole')
 
-var app     = require('express')()
-  , server  = require('http').createServer(app)
+/*!
+ * Module dependencies.
+ */
 
-app.set('server', server)
+if (process.env.NODE_ENV !== 'production') {
+  require('rconsole')
+}
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , connect = require('connect')
+  , cookie = require('cookie')
+  , redis = require('redis')
+  , ejs = require('ejs')
+  , sio = require('socket.io')
+  , io = sio.listen(server)
+  , parseCookie = connect.utils.parseSignedCookies
+  , RedisStore = require('connect-redis')(express)
+  , Engine = require('./lib/engine')
+  , config = require('./config')
 
-require('./config')(app)
+/*!
+ * Server configuration
+ */
 
-require('./pages')(app)
+// Auto config each environment setting
+for (var env in config) {
+  var conf = config[env]
+  app.configure(env, function() {
+    for (var key in conf) {
+      app.set(key, conf[key])
+    }
+  })
+}
 
-require('./realtime')(app)
-
-server.listen(app.settings.port, function () {
-  console.log('Express server started on port: ' + app.settings.port)
+app.configure(function () {
+  app.set('io', io)
+  app.set('db', redis.createClient(app.settings.redis.port, app.settings.redis.host))
+  app.settings.db.auth(app.settings.redis.auth)
+  app.set('sessionStore', new RedisStore({client: app.settings.db}))
+  app.set('sessionSecret', 'IvIVKmFkjE!!a3fP6C38%m%C0%n094bpGnn73GrJU5$oET6!tI^a4pmFs7X3!Ue^')
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(express.cookieParser())
+  app.use(express.session({
+      store: app.settings.sessionStore
+    , secret: app.settings.sessionSecret
+  }))
+  app.use(express.errorHandler({ 
+    dumpExceptions: true
+  , showStack: true
+  }))
+  app.use(express.static(__dirname + '/public'))
+  app.use(app.router)
 })
 
+/*!
+ * Socket.io configuration
+ */
 
-// Catch all exceptions
-// process.on('uncaughtException', function(err) {
-//   console.log('UNCAUGHT EXCEPTION: ', err.message, err.type, err.stack)
-// })
+io.configure(function () {
+  var settings = app.settings
+    , redisConf = settings.redis
+    , RedisStore = sio.RedisStore
+    , redisPub = redis.createClient(redisConf.port, redisConf.host)
+    , redisSub = redis.createClient(redisConf.port, redisConf.host)
+  
+  redisPub.auth(redisConf.auth)
+  redisSub.auth(redisConf.auth)
+
+  io.set('store', new RedisStore({
+    redis: redis
+  , redisPub: redisPub
+  , redisSub: redisSub
+  , redisClient: settings.db
+  }))
+  
+  io.disable('log');
+  io.set('authorization', function (data, callback) {
+    if (!data.headers.cookie) {
+      return callback('No cookie', false)
+    }
+    var parsedCookie = parseCookie(cookie.parse(decodeURIComponent(data.headers.cookie)), app.settings.sessionSecret)
+    settings.sessionStore.get(parsedCookie['connect.sid'], function (err, session) {
+      if (err || !session) {
+        return callback('Error', false)
+      }
+      data.session = session
+      data.session.id = parsedCookie['connect.sid']
+      callback(null, true)
+    })
+  })
+})
+
+app.set('server', server)
+app.set('engine', new Engine(app.settings.db, app.settings.io))
+require('./pages')(app)
+
+/*!
+ * Start server
+ */
+
+server.listen(app.settings.port, function() {
+  console.log('Server started on port: ' + app.settings.port)
+})
